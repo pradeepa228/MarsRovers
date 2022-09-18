e7c04844a3f5d17efd6c0c8fd26af21e5d12a412
pradeepa228 <111800846+pradeepa228@users.noreply.github.com>
2022-09-18 07:23:30 +0100

    Failure test case for invalid direction

--- MRover/Position.cs
+++ MRover/Position.cs
@@ -8,15 +8,14 @@ namespace MRover
 {
     public class Position
     {
-        public int X { get; private set; }
-        public int Y { get; private set; }
-        public char Direction { get; private set; }
-
-        private const string directionToMove = "N|E|S|W";
+           public int X { get; private set; }
+           public int Y { get; private set; }
+           public char Direction { get; private set; }
+           string stringDirection = "NEWS";
 
         public Position(int x, int y, char direction)
         {
-            if (x < 0 || y < 0 || !Regex.IsMatch(direction.ToString(), directionToMove))
+            if (x < 0 || y < 0 || !stringDirection.Contains(direction))
             {
                 throw new ArgumentException("Position can not have negative values or invalid direction.");
             }
@@ -25,7 +24,9 @@ namespace MRover
                 X = x;
                 Y = y;
                 Direction = direction;
+                Direction = direction;
             }
         }
+        
     }
-    }
+}
